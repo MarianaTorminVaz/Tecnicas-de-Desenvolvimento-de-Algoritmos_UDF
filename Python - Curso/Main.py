9ef88a95d443c77be90313a6f7386990a9206562
Mariana Tormin <vazmarianat@gmail.com>
2025-10-30 20:43:52 -0300

    Mini projeto feito durante Curso do Bradesco.
    Curso - Desenvolvimento Orientado a Objetos Utilizando a Linguagem Python.

---
+++ Python - Curso/Main.py
@@ -0,0 +1,16 @@
+class Main:
+    pass
+
+from Cliente import Cliente
+
+from Conta import Conta
+
+c1= Cliente("João", "114444-2222")
+conta= Conta(c1.get_nome(), 1222)
+
+conta.deposita(100)
+conta.saque(50)
+conta.extrato()
+
+print(conta.titular, " Número: ",conta.numero, " Seu saldo: ", conta.saldo)
+
